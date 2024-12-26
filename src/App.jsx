b9f37ab2f8563b8d68840852385447d5dc0daaf0
Christian Ivan Cazarin de Anda <c.cazarin@outlook.com>
2024-12-25 19:58:49 -0600

    Llamada a la API para ver como funciona

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-function App() {
+import { useEffect, useState } from 'react';
 
-  return 
+function App() {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.weatherapi.com/v1/current.json?key=795ffba897b347c984f215602242412&q=20.768703,-103.443375')
+      .then(response => response.json())
+      .then(datos => {
+        setWeather(datos);
+      })
+      .catch(error => {
+        console.error('Hubo un error:', error);
+      });
+  }, []);
+
+  return (
     <div>
-      Weather app
+      {weather ? (
+        <div>
+          <h2>{weather.location.name}</h2>
+          <p>{weather.current.temp_c}°C</p>
+          <p>{weather.current.condition.text}</p>
+          <p>{weather.current.humidity}</p>
+        </div>
+      ) : (
+        <p>Cargando...</p> 
+      )}
     </div>
-  
+  );
 }
 
-export default App
+export default App;
